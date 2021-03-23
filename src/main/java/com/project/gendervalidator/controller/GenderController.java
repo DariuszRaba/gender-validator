3316c2a59c85b211d43121fa17f5f952ddb16d7d
Dariusz Raba <rabadariusz@gmail.com>
2021-03-23 20:34:34 +0100

    Add service and controller connection

--- src/main/java/com/project/gendervalidator/controller/GenderController.java
+++ src/main/java/com/project/gendervalidator/controller/GenderController.java
@@ -1,17 +1,21 @@
 package com.project.gendervalidator.controller;
 
+import com.project.gendervalidator.service.GenderService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/gender")
 public class GenderController {
 
+    private final GenderService genderService;
+
     @GetMapping("/tokens")
     public String getAllTokens(){
-        return "testing";
+        return genderService.getAllTokens();
     }
 }
---
+++ src/main/java/com/project/gendervalidator/service/GenderService.java
@@ -0,0 +1,17 @@
+package com.project.gendervalidator.service;
+
+import com.project.gendervalidator.dao.TxtTokenFilesDAO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class GenderService {
+
+    private final TxtTokenFilesDAO txtTokenFilesDAO;
+
+    public String getAllTokens(){
+        return txtTokenFilesDAO.getMalesTokens();
+    }
+}
