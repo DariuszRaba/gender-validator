47448c077833b3e11d45e89ea60483e3da0de344
Dariusz Raba <rabadariusz@gmail.com>
2021-03-23 16:58:55 +0100

    Initial commit

---
+++ src/main/java/com/project/gendervalidator/GenderValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.project.gendervalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gendervalidator/GenderValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gendervalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
