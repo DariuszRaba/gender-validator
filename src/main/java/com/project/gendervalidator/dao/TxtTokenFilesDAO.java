cc6d52f895ecfefaa673fb5bea0e4989e8d92a43
Dariusz Raba <rabadariusz@gmail.com>
2021-03-25 19:54:08 +0100

    Add txtToken class refactor

--- src/main/java/com/project/gendervalidator/dao/TxtTokenFilesDAO.java
+++ src/main/java/com/project/gendervalidator/dao/TxtTokenFilesDAO.java
@@ -3,46 +3,41 @@ package com.project.gendervalidator.dao;
 import com.project.gendervalidator.exception.ResourceException;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class TxtTokenFilesDAO implements TokensDAO {
 
-
     @Override
     public String getMalesTokens() {
         try {
-            Path malePath = Paths.get(Objects.requireNonNull(getClass().getClassLoader()
-                    .getResource("male-names.txt")).toURI());
-            return getAllTokensFromFile(malePath);
-        } catch (IOException | URISyntaxException e) {
+            String NAME_OF_MALE_TOKENS_FILE = "male-names.txt";
+            return getAllTokensFromFile(NAME_OF_MALE_TOKENS_FILE);
+        } catch (NullPointerException ex) {
             throw new ResourceException("Male");
         }
     }
 
     @Override
     public String getFemalesTokens() {
-        try {
-            Path femalePath = Paths.get(Objects.requireNonNull(getClass().getClassLoader()
-                    .getResource("female-names.txt")).toURI());
-            return getAllTokensFromFile(femalePath);
-        } catch (IOException | URISyntaxException e) {
+       try {
+           String NAME_OF_FEMALE_TOKENS_FILE = "female-names.txt";
+           return getAllTokensFromFile(NAME_OF_FEMALE_TOKENS_FILE);
+        } catch (NullPointerException ex) {
             throw new ResourceException("Female");
         }
     }
 
-    private String getAllTokensFromFile(Path filePath) throws IOException {
-        Stream<String> lines = Files.lines(filePath);
-        String tokenNames = lines.collect(Collectors.joining("\n"));
-        lines.close();
-        return tokenNames;
+    private String getAllTokensFromFile(String fileName) throws NullPointerException {
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(fileName);
+        return new BufferedReader(
+                new InputStreamReader(resourceAsStream, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
     }
 
 }
